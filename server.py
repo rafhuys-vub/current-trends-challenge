b4dbfae80ceacc74b52924df9e5bf6be8200363c
raf huys <raf.huys@klarrio.com>
2024-05-15 20:44:17 +0200

    improve prompt

--- server.py
+++ server.py
@@ -40,37 +40,13 @@ with open('data/data.jsonl') as f:
 
 def question(q):
     return """
-    [INST] 
-    <<SYS>> 
-    You are used by a software package as an API. So you should only respond with a direct answer.
-    
-    You should translate English sentences into sentences of a reduced English vocabulary, such that these result sentences can serve as input for an AutoML system.
-    The reduced vocabulary consists of the following tokens: load, dataset, calculate, volume, cluster, clustering, regression, data, count, clusters, a, and, perform, using, id, surface, height, mass, density, columns.
-
-    The sentences are queries or instructions regarding a dataset consisting of four columns: id (integer), surface (in square meters, float), height (in meters, float), mass (in kilograms, float). The 'id', 'surface', 'height' and 'mass' tokens of the reduced vocabulary correspond to these column names.
-    These columns describe the id, surface, height and mass of each object. The volume of the object is not a column but can be calculated as the product of the surface and height. Density of the object can be calculated by dividing the mass by the volume of the object.
-
-    You shouldn't do any calculations, only translate sentences to the reduced vocabulary while considering the relations between the colums of the dataset, as detailed out in the previous 2 sentences.
-    From the input sentence you should deduce the type of machine learning algorithm. It should be either regression, clustering or none at all. 
-
-    As an example, a sentence such as `Given the objects dataset, how many types of objects can be found, according to their volume?` would be translated into `load dataset, calculate volume using the surface and height columns, cluster and count clusters`. In this case the required ML algorithm is clustering as there is no column indicating the object type. Instead, the object type is deduced by performing clustering, a type of unsupervised learning.
-    Another example:  "Train a model that can estimate density based on surface and mass given the objects dataset." is translated into "load dataset, calculate density using surface and mass columns, and perform regression". In this case the required ML algorithm is regression as density is an object property that is calculated by other columns and can thus be learned by regression, a supervised learning algorithm. 
-        <</SYS>>
-        
-
-    Can you translate the sentence "%s" and reply only with the translation?
-    [/INST]
-    """ %(q)\
-    
-def question_examples(q, exs):
-    return """
     <s>[INST] <<SYS>> 
     You are used by a software package as an API. So you should only respond with a direct answer.
     
     You should translate English sentences into sentences of a reduced English vocabulary, such that these result sentences can serve as input for an AutoML system.
-    The reduced vocabulary consists of the following tokens: load, dataset, calculate, volume, cluster, clustering, regression, data, count, clusters, a, and, perform, using, id, surface, height, mass, density, columns.
+    The reduced vocabulary only consists of the following words: load, dataset, calculate, volume, cluster, clustering, regression, data, count, clusters, a, and, perform, using, id, surface, height, mass, density, columns.
 
-    The sentences are queries or instructions regarding a dataset consisting of four columns: id (integer), surface (in square meters, float), height (in meters, float), mass (in kilograms, float). The 'id', 'surface', 'height' and 'mass' tokens of the reduced vocabulary correspond to these column names.
+    The sentences are queries or instructions regarding a dataset consisting of four columns: id (integer), surface (in square meters, float), height (in meters, float), mass (in kilograms, float). The 'id', 'surface', 'height' and 'mass' words of the reduced vocabulary correspond to these column names.
     These columns describe the id, surface, height and mass of each object. The volume of the object is not a column but can be calculated as the product of the surface and height. Density of the object can be calculated by dividing the mass by the volume of the object.
 
     You shouldn't do any calculations, only translate sentences to the reduced vocabulary while considering the relations between the colums of the dataset, as detailed out in the previous 2 sentences.
@@ -83,6 +59,35 @@ def question_examples(q, exs):
     Train a model that can estimate density based on surface and mass given the objects dataset.[/INST] load dataset, calculate density using surface and mass columns, and perform regression. </s><s>[INST]
     %s[/INST]
     """ %(q)\
+
+def flatten(xxs):
+    return [x for xs in xxs for x in xs]
+    
+def print_embeddings(documents, metadatas):
+    examples=[]
+    for doc, meta in list(zip(documents, metadatas)):
+        examples.append("{doc} [/INST] {translation} </s><s>[INST]".format(doc=doc, translation=meta['translation']))
+    return '\n'.join(examples)
+    
+def question_examples(q, exs):
+    return """
+    <s>[INST] <<SYS>> 
+    You are used by a software package as an API. So you should only respond with a direct answer.
+    
+    You should translate English sentences into sentences of a reduced English vocabulary, such that these result sentences can serve as input for an AutoML system.
+    The reduced vocabulary only consists of the following words: load, dataset, calculate, volume, cluster, clustering, regression, data, count, clusters, a, and, perform, using, id, surface, height, mass, density, columns.
+
+    The sentences are queries or instructions regarding a dataset consisting of four columns: id (integer), surface (in square meters, float), height (in meters, float), mass (in kilograms, float). The 'id', 'surface', 'height' and 'mass' words of the reduced vocabulary correspond to these column names.
+    These columns describe the id, surface, height and mass of each object. The volume of the object is not a column but can be calculated as the product of the surface and height. Density of the object can be calculated by dividing the mass by the volume of the object.
+
+    You shouldn't do any calculations, only translate sentences to the reduced vocabulary while considering the relations between the colums of the dataset, as detailed out in the previous 2 sentences.
+    From the input sentence you should deduce the type of machine learning algorithm. It should be either regression, clustering or none at all. 
+    
+    You should reply _only_ with the translated sentence using the reduced vocabulary. Do not include any additional text or instructions. Next are a few examples of how to translate the input sentence.
+    <</SYS>>
+    %s
+    %s[/INST]
+    """ %(exs, q)\
 
 app.logger.info(f"Preheat embeddings with a dummy query")
 
@@ -98,12 +103,17 @@ def chat():
     data = request.get_json()
     q = data.get('question')
     app.logger.info(f"Received question: {q}")
-    similar_docs= collection.query(
+    result = collection.query(
         query_texts=[q],
         n_results=6,
     )
-    app.logger.info(f"Found {len(similar_docs['documents'])} similar documents")
-    response = ollama.chat(model='llama2', messages=[{'role': 'user', 'content': question_examples(q, [])}])
+    print(result)
+    docs = flatten(result['documents'])
+    metadatas = flatten(result['metadatas'])
+    examples_string = print_embeddings(docs, metadatas)
+    
+    app.logger.info(f"Found similar documents: {examples_string}")
+    response = ollama.chat(model='llama2', messages=[{'role': 'user', 'content': question_examples(q, examples_string)}])
     return jsonify(response)
 
 if __name__ == '__main__':
